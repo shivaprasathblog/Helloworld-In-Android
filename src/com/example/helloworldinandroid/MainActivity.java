47540280dad482f21fc23fb978573fc30b4e4eac
Shivaprasath <shivaprasathblog@users.noreply.github.com>
2015-11-28 14:38:23 +0530

    Update MainActivity.java

--- src/com/example/helloworldinandroid/MainActivity.java
+++ src/com/example/helloworldinandroid/MainActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
+		/**"setContentView" is used for 
+		creating a window for you in which you can place your UI(Ex : Button , Textview ,etc.)**/
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
